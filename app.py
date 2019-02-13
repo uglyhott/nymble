c36ab48d3e030b80b36d8fa59da8b89b6a6c5ee9
uglyhott <liam.bt@gmail.com>
2019-02-13 21:07:09 +1300

    Check each created term against Owblbot Dictionary API - currently not saving checked words beyond the single letter concatenated slices.

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import requests
+import itertools
 
+API_URL = 'https://owlbot.info/api/v2/dictionary/'
 # user_in_string = input("Enter words separated by a space: ")
 
-input_list = ['acronym', 'creator', 'tool']# [word.strip() for word in user_in_string.split()]
+input_list = ['acronym', 'creator', 'tool']  # [word.strip() for word in user_in_string.split()]
 print(input_list)
 
 # Creates the dictionary where the synonyms will be linked to the inputted term.
@@ -17,3 +19,47 @@ for word in input_list:
     linked_synonyms[word] = synonym_list
 
 print(linked_synonyms)
+
+# Takes each synonym value and finds the product of all the rest.
+# TODO: include the original user terms in the itertools.product
+product_of_all_synonyms = list(itertools.product(linked_synonyms['acronym'], linked_synonyms['creator'], linked_synonyms['tool']))
+
+# Inputs a list of synonyms in sets of 3 and slices them according to a desired count.
+# TODO: Refactor to allow for more than than 3 terms.
+def sliced_synonym(list_of_synonyms, count):
+    concatenated_synonyms = {}
+    for term in list_of_synonyms:
+        sliced = term[0][:count] + term[1][:count] + term[2][:count]
+        concatenated_synonyms[sliced] = term
+    return concatenated_synonyms
+
+
+single_sliced = sliced_synonym(product_of_all_synonyms, 1)
+double_sliced = sliced_synonym(product_of_all_synonyms, 2)
+triple_sliced = sliced_synonym(product_of_all_synonyms, 3)
+
+print(single_sliced)
+print(double_sliced)
+print(triple_sliced)
+
+
+def dictionary_check(created_words):
+    checked_words = {}
+    for key in created_words.keys():
+        s = requests.get(API_URL + key)
+        if not s.json():
+            print(f'{key} is not a word.')
+            continue
+        print(f'{key} is an English word.')
+        checked_words[key] = created_words[key]
+    return checked_words
+
+
+check1 = dictionary_check(single_sliced)
+check2 = dictionary_check(double_sliced)
+check3 = dictionary_check(triple_sliced)
+print(check1)
+
+# TODO: review why these two print empty dictionaries.
+print(check2)
+print(check3)
